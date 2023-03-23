11ee9f528578b3843f6b02a03383799e6e6fb7fb
Yassaraj <yassarajay@gmail.com>
2023-03-23 19:53:11 +0530

    profile page initial commit

---
+++ src/pages/ProfilePage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import TopNavbar from "../components/common/TopNavBar";
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+
+export default function ProfilePage(){
+    return(
+        <>
+            <TopNavbar
+            title="Profile"
+            icon={<AccountCircleIcon/>}
+            />  
+        </>
+    )
+}
